08133e708f4ad19a5615fc242c7e8c1a19bb5ed1
Andrew Smith <smithandrewj@outlook.com>
2020-07-03 14:10:35 -0700

    add first routes

---
+++ routes/api/lists.js
@@ -0,0 +1,38 @@
+// { id: 1, name: 'Quarantine Mix' },
+// { id: 2, name: 'Best of Pavement' },
+// { id: 3, name: 'Workout Songs' },
+const express = require('express');
+const router = express.Router();
+
+const List = require('../../models/List');
+
+/**
+ * @route   GET /api/lists
+ * @desc    Retrieve all lists
+ * @access  Public
+ */
+router.get('api/lists', async (req, res) => {
+  try {
+    const lists = await List.find();
+    res.json(lists);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+router.post('api/lists', async (req, res) => {
+  try {
+    const newList = new List({
+      name: req.body.name,
+      description: req.body.description,
+      tracks: req.body.tracks,
+    });
+
+    const list = await newList.save();
+    res.json(list);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
-app.get('/api/lists', (req, res) => {
-  const lists = [
-    { id: 1, name: 'Quarantine Mix' },
-    { id: 2, name: 'Best of Pavement' },
-    { id: 3, name: 'Workout Songs' },
-  ];
+// Connect Database
+connectDB();
 
-  res.json(lists);
-});
+// Init Middleware
+// app.use(cors());
+app.use(express.json());
 
-const port = 5000;
+// Define Routes
+app.use('/api/lists', require('./routes/api/lists'));
 
-app.listen(port, () => `Server running on port ${port}`);
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
